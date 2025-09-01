fafde66ca8d905f15ba55afd4cfdbb9cf7db3a7f
Sean Bien <pandemonium0225@gmail.com>
2025-09-01 10:24:11 +0800

    Initial commit: Forkify recipe application with search, pagination, and servings update

---
+++ src/js/controller.js
@@ -0,0 +1,78 @@
+import * as model from './model.js';
+import recipeView from './view/recipeView.js';
+import searchView from './view/searchView.js';
+import resultsView from './view/resultsView.js';
+import paginationView from './view/paginationView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// NEW API URL (instead of the one shown in the video)
+// https://forkify-api.jonas.io
+
+///////////////////////////////////////
+if (module.hot) module.hot.accept();
+
+const controlRecipes = async function () {
+  try {
+    // resultsView.renderSpinner();
+    // Get ID from URL
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
+    // 1) loading Recipe
+    await model.loadRecipe(id);
+    // 2) Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // 1) Get Search Query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load Search Results
+    await model.loadSearchResults(query);
+    // 3) Render Search Results
+    resultsView.render(model.getSearchResultsPage(1));
+
+    // 4) Render initial pagination
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const controlPagination = function (gotoPage) {
+  // 1) Render NEW Search Results
+  resultsView.render(model.getSearchResultsPage(gotoPage));
+
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // 1) Update the recipe servings (in state)
+  model.updateServings(newServings);
+  // 2) Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+
+init();
